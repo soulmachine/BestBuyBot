ffe70c8381c70c96d6361dc083afa414775a6b23
soulmachine <soulmachine@gmail.com>
2022-02-17 22:16:08 -0800

    First version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BestBuyBot
+
+## How to run
+
+1. Start the Chrome browser with arguments `--remote-debugging-port=9222 --no-first-run --no-default-browser-check`
+2. Open <www.bestbuy.com> and login with your username and password, so that you don't need to login in your code
+3. Open `http://127.0.0.1:9222/json/version` in the browser and copy the value of `webSocketDebuggerUrl`
+4. Set the value to `WS_ENDPOINT` environment variable
+5. Type the command `node ./bot.js`
--- bot.jsx
+++ bot.jsx
@@ -1,295 +1,77 @@
-const rp = require('request-promise');
-const puppeteer = require('puppeteer');
-const pptr = require('puppeteer-core');
-var HTMLParser = require('node-html-parser');
-var nodemailer = require('nodemailer');
-var nodeoutlook = require('nodejs-nodemailer-outlook')
-const axios = require('axios');
-var http = require('http');
-var https = require('https');
+const pptr = require("puppeteer-core");
+const fs = require("fs");
 
-require('dotenv').config()
-http.globalAgent.maxSockets = 1;
-https.globalAgent.maxSockets = 1;
+require("dotenv").config();
 
-// localhost:9222/json/version
+// Make sure you've logged in www.bestbuy.com already in the Chrome of your computer.
 
-const url_test = 'https://www.bestbuy.ca/en-ca/product/apple-apple-lightning-to-3-5mm-headphone-jack-adapter-mmx62am-a/10487473';
+const URL_TEST =
+  "https://www.bestbuy.com/site/apple-lightning-to-3-5mm-headphone-adapter-white/5622278.p?skuId=5622278";
+const URL_RTX_3090 =
+  "https://www.bestbuy.com/site/nvidia-geforce-rtx-3090-24gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429434.p?skuId=6429434";
 
-const url_PS5 = 'https://www.bestbuy.ca/en-ca/product/playstation-5-digital-edition-console-online-only/14962184';
-const url_3060_ASUS_ROG = 'https://www.bestbuy.ca/en-ca/product/asus-rog-strix-geforce-rtx-3060-oc-12gb-gddr6x-video-card/15309514';
-const url_3060_ASUS_TUF = 'https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-geforce-rtx-3060-oc-12gb-gddr6x-video-card/15309513';
-const url_3060_EVGA = 'https://www.bestbuy.ca/en-ca/product/evga-nvidia-geforce-rtx-3060-xc-12gb-dddr6-video-card/15318940';
-const url_3060_MSI_VENTUS2X = 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3060-ventus-2x-oc-12gb-gddr6-video-card/15317226';
-const url_3060_MSI_VENTUS3X = 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3060-ventus-3x-12gb-gddr6-video-card/15324508';
-const url_3060_ZOTAC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3060-twin-edge-12gb-gddr6-video-card/15309504';
-const url_3060_ZOTAC_OC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3060-twin-edge-oc-12gb-gddr6-video-card/15309503'
-const url_3060TI_EVGA = 'https://www.bestbuy.ca/en-ca/product/evga-nvidia-geforce-rtx-3060-ti-ftw3-ultra-8gb-gddr6-video-card/15229237';
-const url_3060TI_MSI = 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3060-ti-ventus-2x-oc-8gb-gddr6-video-card/15178453';
-const url_3060TI_ZOTAC = 'https://www.bestbuy.ca/en-ca/product/zotac-geforce-rtx-3060-ti-twin-edge-oc-8gb-gddr6-video-card/15178452';
-const url_3060TI_NVIDIA = 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3060-ti-8gb-gddr6-video-card/15166285'
-const url_3070_ASUS_DUAL = 'https://www.bestbuy.ca/en-ca/product/asus-rog-strix-nvidia-geforce-rtx-3070-oc-8gb-gddr6-video-card/15053085';
-const url_3070_ASUS_ROG = 'https://www.bestbuy.ca/en-ca/product/asus-dual-nvidia-geforce-rtx-3070-2x-oc-8gb-gddr6-video-card/15053086';
-const url_3070_ASUS_TUF = 'https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-nvidia-geforce-rtx-3070-2x-oc-8gb-gddr6-video-card/15053087';
-const url_3070_EVGA = 'https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3070-xc3-ultra-8gb-gddr6-video-card/15147122';
-const url_3070_MSI = 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3070-ventus-3x-oc-8gb-gddr6-video-card/15038016';
-const url_3070_NVIDIA = 'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017';
-const url_3070_ZOTAC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3070-twin-edge-8gb-gddr6x-video-card/15000079';
-const url_3070_ZOTAC_OC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3070-twin-edge-oc-8gb-gddr6x-video-card/15000078';
-const url_3080_ASUS_ROG = 'https://www.bestbuy.ca/en-ca/product/asus-rog-strix-nvidia-geforce-rtx-3080-10gb-gddr6x-video-card/14954116';
-const url_3080_ASUS_TUF = 'https://www.bestbuy.ca/en-ca/product/asus-tuf-gaming-nvidia-geforce-rtx-3080-10gb-gddr6x-video-card/14953248';
-const url_3080_EVGA = 'https://www.bestbuy.ca/en-ca/product/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6x-video-card/15084753';
-const url_3080_MSI = 'https://www.bestbuy.ca/en-ca/product/msi-nvidia-geforce-rtx-3080-ventus-3x-10gb-gddr6x-video-card/14950588';
-const url_3080_ZOTAC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3080-trinity-10gb-gddr6x-video-card/14953249';
-const url_3080_ZOTAC_OC = 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3080-trinity-oc-10gb-gddr6x-video-card/15000077';
-
-const url_cart = 'https://www.bestbuy.ca/en-ca/basket'
-const url_paypal = "https://www.bestbuy.ca/checkout/?qit=1#/en-ca/shipping/ON/M2N?expressPaypalCheckout=true"
-const url_api = 'http://api.bestbuy.ca/availability/products?callback=apiAvailability&accept-language=en&accept=application%2Fvnd.bestbuy.standardproduct.v1%2Bjson&maxlos=3&'
-
-
-const names =  {
-  '14962184': 'Digital PS5',
-  '15309514': '3060 ASUS ROG',
-  '15309513': '3060 ASUS TUF',
-  '15318940': '3060 EVGA', 
-  '15317226': '3060 MSI VENTUS 2x',
-  '15324508': '3060 MSI VENTUS 3x', 
-  '15309504': '3060 ZOTAC',
-  '15309503': '3060 ZOTAC OC',
-  '15229237': '3060TI EVGA',
-  '15178453': '3060TI MSI',
-  '15166285': '3060TI NVIDIA',
-  '15178452': '3060TI ZOTAC',
-  '15053085': '3070 ASUS DUAL',
-  '15053086': '3070 ASUS ROG',
-  '15053087': '3070 ASUS TUF',
-  '15147122': '3070 EVGA', 
-  '15038016': '3070 MSI', 
-  '15078017': '3070 NVIDIA',
-  '15000079': '3070 ZOTAC',
-  '15000078': '3070 ZOTAC OC',
-  '14954116': '3080 ASUS ROG',
-  '14953248': '3080 ASUS TUF',
-  '15084753': '3080 EVGA', 
-  '14950588': '3080 MSI',
-  '14953249': '3080 ZOTAC',
-  '15000077': '3080 ZOTAC OC',
-  '10487473': 'TEST ITEM'
-}
-
+const URL_CART = "https://www.bestbuy.com/cart";
 
 var chromeOptions = {
   headless: false,
-  executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+  // executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+  executablePath:
+    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
   browserWSEndpoint: process.env.WS_ENDPOINT,
-  defaultViewport: null
-}
-
-var transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com',
-  port: 587,
-  secureConnection: false,
-  tls: {
-     ciphers:'SSLv3'
-  }, 
-  auth: {
-    user: process.env.TO_EMAIL,
-    pass: process.env.TO_PASSWORD
-  }
-});
-
-
-var mailOptions = {
-  from: process.env.TO_EMAIL,
-  to: process.env.TO_EMAIL,
-  subject: 'GPU is in stock',
-  text: url_test
+  defaultViewport: null,
 };
 
-function intervalManager(flag, link, time=3000) {
-  var interval_id = link + "-interval"
-
-  if (flag) global[interval_id] = setInterval(() => {checkStatus(link, time)}, time)
-  else {
-    clearInterval(global[interval_id])
-  }
-}
-
-async function checkStatus (curr_url, interval_length) {
-  intervalManager(false, curr_url)
-
-  var searchOptions = {
-    uri: curr_url,
-    headers: {
-      'Connection': 'keep-alive',
-      'Accept-Encoding': '',
-      'Accept-Language': 'en-US,en;q=0.8'
-    }
-  }
-
-  function check_api (curr_url) {
-    return axios.get(url_api + 'skus=' + curr_url.split("/")[6])
-    .then(
-      (response) => {
-        var availability_string = JSON.parse(response.data.replace('apiAvailability(', '').replace(');', ""))
-        return {
-          api_status: availability_string.availabilities[0].shipping.status,
-          api_quantity: availability_string.availabilities[0].shipping.quantityRemaining,
-          api_purchasable: availability_string.availabilities[0].shipping.purchasable
-        }
-      }
-    )
-    .catch(
-      (error) => {
-        console.log(error)
-      }
-    )
-  }
-
-  async function check_page (curr_url) {
-    await rp(searchOptions)
-    .then((html) => {
-      var root = HTMLParser.parse(html)
-      var availability_text = root.querySelector(".shippingAvailability_2RMa1").textContent
-      var gpu_type = names[curr_url.split("/")[6]]
-      const is_disabled = root.querySelector('#test > button').classList.contains("disabled_LqxUL")
-      const is_available = !(["Coming soon", "Sold out online"].includes(availability_text))
-      console.log(gpu_type +  '  -------   ' + availability_text)
-
-      if (!(is_disabled) || is_available)   {
-        sendEmail(curr_url)
-        openBrowser(curr_url)
-      }
-      else {
-        intervalManager(true, curr_url, interval_length)
-      }
-    })
+async function checkStatus(itemUrl, interval) {
+  openPage(itemUrl)
     .catch((error) => {
-      console.log(error)
-      // intervalManager(true, curr_url, invterval_length)
+      console.log(error);
     })
-  }
-
-
-  check_api(curr_url)
-  .then((api_data) => {
-    const { api_status, api_quantity, api_purchasable} = api_data
-    const is_status = ["ComingSoon", "SoldOutOnline", 'NotAvailable'].includes(api_status)
-    const has_quantity = api_quantity > 0
-    const is_purchasable = api_purchasable
-
-    var gpu_type = names[curr_url.split("/")[6]]
-    console.log(gpu_type +  '  -------   ' + api_status)
-
-    if (has_quantity) {
-      sendEmail(curr_url, api_quantity)
-      openBrowser(curr_url)
-    }
-    else {
-      intervalManager(true, curr_url, interval_length)
-    }
-  })
-  .catch( (error) => {
-    console.log(error)
-    intervalManager(true, curr_url, interval_length)
-  })
-  // check_page(curr_url)
-
+    .then((success) => {
+      if (success) {
+        console.log("Succeeded");
+      } else {
+        console.log("Sleeping...");
+        setTimeout(() => checkStatus(itemUrl, interval), interval);
+      }
+    });
 }
 
-
-async function openBrowser (curr_url) {
+async function openPage(itemUrl) {
+  console.log(new Date(), itemUrl);
   const browser = await pptr.connect(chromeOptions);
   const page = await browser.newPage();
-  await page.goto(curr_url);
-  var cookies = await page.cookies()
 
-  for (cookie of cookies) {
-    if (cookie.domain === '.bestbuy.ca' || cookie.domain === 'www.bestbuy.ca') {
-      await page.deleteCookie({
-        name : cookie.name,
-        domain : cookie.domain,
-      })
-    }
+  await page.goto(itemUrl);
+  const button = await page.waitForSelector("button.add-to-cart-button");
+  const buttonText = await button.evaluate((el) => el.textContent);
+  console.log(buttonText);
+  if (buttonText !== "Add to Cart") {
+    await page.close();
+    return false;
   }
+  await button.click();
 
-  await page.focus("#test > button")
-  await page.keyboard.type('\n');
-  await page.waitForTimeout(5000)
-  await page.goto(url_cart);
-  // const response = await page.goto("https://www.bestbuy.ca/checkout/?qit=1#/en-ca/shipping/ON/M2N?expressPaypalCheckout=true");
-  const reponse = await page.goto("https://www.bestbuy.ca/identity/global/signin?redirectUrl=https%3A%2F%2Fwww.bestbuy.ca%2Fcheckout%2F%3Fqit%3D1%23%2Fen-ca%2Fshipping%2FON%2FL6W&amp;lang=en-CA&amp;contextId=checkout")
-  await page.type('#username', process.env.TO_EMAIL)
-  await page.type('#password', process.env.BESTBUY_PASSWORD)
-  const signIn = await page.$('#signIn > div > button')
-  await signIn.evaluate( signIn => signIn.click() )
-  await page.waitForSelector("#cvv")
-  await page.type('#cvv', process.env.CVV)
+  await page.goto(URL_CART);
+  const checkout = await page.waitForSelector(
+    "div.checkout-buttons__checkout > button"
+  );
+  await checkout.click();
 
-  // SIGN IN, BECAREFUL WHEN UNCOMMENTING
-  const placeOrder = await page.$('#posElement > section > section.cost-sum-section > button')
-  await placeOrder.evaluate( placeOrder => placeOrder.click())
+  await page.waitForSelector("#cvv");
+  await page.type("#cvv", process.env.CVV);
+
+  const placeOrderButton = await page.waitForSelector(
+    "div.contact-card__order-button > div > div.payment__order-summary > button"
+  );
+  await placeOrderButton.click();
+  await page.close();
+  return true;
 }
 
-const sendEmail = async (curr_url, quantity) => {
-
-  mailOptions.subject = names[curr_url.split("/")[6]] + " IS HERE. QUANTITY = " + quantity + "."
-  mailOptions.text = curr_url; 
-  let info = await transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-} 
-
-const interval_length = 3000
-
-intervalManager(true, url_3060_ASUS_ROG, interval_length)
-intervalManager(true, url_3060_ASUS_TUF, interval_length)
-intervalManager(true, url_3060_EVGA, interval_length)
-intervalManager(true, url_3060_MSI_VENTUS2X, interval_length)
-intervalManager(true, url_3060_MSI_VENTUS3X, interval_length)
-intervalManager(true, url_3060_ZOTAC, interval_length)
-intervalManager(true, url_3060_ZOTAC_OC, interval_length)
-intervalManager(true, url_3060TI_EVGA, interval_length)
-intervalManager(true, url_3060TI_MSI, interval_length)
-intervalManager(true, url_3060TI_NVIDIA, interval_length)
-intervalManager(true, url_3060TI_ZOTAC, interval_length)
-intervalManager(true, url_3070_ASUS_DUAL, interval_length)
-intervalManager(true, url_3070_ASUS_ROG, interval_length)
-intervalManager(true, url_3070_ASUS_TUF, interval_length)
-intervalManager(true, url_3070_EVGA, interval_length)
-intervalManager(true, url_3070_NVIDIA, interval_length)
-intervalManager(true, url_3070_MSI, interval_length)
-intervalManager(true, url_3070_ZOTAC, interval_length)
-intervalManager(true, url_3070_ZOTAC_OC, interval_length)
-intervalManager(true, url_3080_ASUS_ROG, interval_length)
-intervalManager(true, url_3080_ASUS_TUF, interval_length)
-intervalManager(true, url_3080_EVGA, interval_length)
-intervalManager(true, url_3080_MSI, interval_length)
-intervalManager(true, url_3080_ZOTAC, interval_length)
-intervalManager(true, url_3080_ZOTAC_OC, interval_length)
-intervalManager(true, url_PS5, interval_length)
-
-//intervalManager(true, url_test, interval_length)
-
-
-
-
- // Paypal
-  // await page.click('#signIn > div > button')
-  // const response = await page.goto("https://www.bestbuy.ca/checkout/?qit=1#/en-ca/payment")
-  // await page.waitForSelector('#payment-submit-btn', { visible: true, timeout: 0 })
-  // await page.click("#payment-submit-btn")
-   // page.waitForNavigation({
-    //   waitUntil: 'networkidle0'
-  // await page.waitForSelector("#root > div > div.x-page-content.container_3Sp8P > div.loader_3thnw > div.loadedContent_2Wp84 > section > div > section > section.cost-sum-section_3pPEp > div.checkoutOptions_1VB1L > div:nth-child(1) > div > div > a")
-  // await page.click("#root > div > div.x-page-content.container_3Sp8P > div.loader_3thnw > div.loadedContent_2Wp84 > section > div > section > section.cost-sum-section_3pPEp > div.checkoutOptions_1VB1L > div:nth-child(1) > div > div > a")
-  // await page.waitForSelector('#payment-submit-btn')
-  // await page.click('#payment-submit-btn')
-  // await page.focus("#root > div > div.x-page-content.container_3Sp8P > div.loader_3thnw > div.loadedContent_2Wp84 > section > div > section > section.cost-sum-section_3pPEp > div:nth-child(3) > div > a")
-  // await page.waitForSelector("#test > button")
-  // await page.click("#test > button")
+(async () => {
+  const interval = 30000; // 30 seconds
+  // URL_TEST, URL_RTX_3090
+  await checkStatus(URL_TEST, interval);
+})().catch((e) => {
+  console.log(e);
+});
